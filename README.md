c98bc7e22654fdd8d265fa769e2c27c8765acafa
Athos de Oliveira <64941377+athosoli@users.noreply.github.com>
2022-07-30 22:10:36 -0300

    teste

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# API-Jogos_olimpcos
+## API Jogos Olímpicos
+
+#### Esta API Tem a finalidade de adicionar dois tipos de competições, 100m rasos: Menor tempo vence
+#### Lançamento de Dardo: Maior distância vence, além disso o usuário poderá consultar as competições em andamento e #### encerradas, visualizar o ranking de atletas, além de registrar o encerramento de uma competição, os usuários deverão se #### autenticar para utilização da API.
+##
+### Link da Documentação:
+#### [API Jogos Olímpicos](https://documenter.getpostman.com/view/15418246/UzdzTR5A)
+##
+### O que Funciona: 
+##### Endpoints:
+##### GET(/competicao/:id, /encerradas, /andamento, /competicao, /resultado, /ranking),
+##### POST(/competicao, /signup, /login, /resultado)
+##### PUT(/competicao/:id)
+##
+### O que Não Funciona:
+#### Nao consegui implementar os testes unitários 
+##
+### Autor:
+#### [Athos de Oliveira](https://github.com/future4code/Athos-Oliveira)
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import {AddressInfo} from "net";
+import express from "express";
+import { userRouter } from "./routes/userRouter";
+import { competicaoRouter } from "./routes/competicaoRouter";
+import { resultadoRouter } from "./routes/ResultadoRouter";
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.post("/signup", userRouter)
+app.post("/login", userRouter)
+app.post("/competicao", competicaoRouter)
+app.get("/competicao/:id", competicaoRouter)
+app.put("/competicao/:id", competicaoRouter)
+app.get("/encerradas", competicaoRouter)
+app.get("/andamento", competicaoRouter)
+app.get("/competicao", competicaoRouter)
+app.post("/resultado", resultadoRouter)
+app.get("/resultado", resultadoRouter)
+app.get("/ranking", resultadoRouter)
+
+app.get("/", competicaoRouter)
+
+const server = app.listen(3000, () => {
+    if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Servidor rodando em http://localhost:${address.port}`);
+    } else {
+      console.error(`Falha ao rodar o servidor.`);
+    }
+  });
